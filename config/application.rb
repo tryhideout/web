87c0593a6b7461ddf7aab13776146cc5c165e720
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-05-13 10:36:48 -0400

    fix: fixed exists usage

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Hideout
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      end if File.exist?(env_file)
     end
     
   end
