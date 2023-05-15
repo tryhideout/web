886f482170b0bc2c1011a98710eb66d61f55dc8f
Harshith <harshithl1777@gmail.com>
2023-05-15 17:58:38 -0400

    chore: change rails app to api only app

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "auth#index"
-
-  get '/', to: 'auth#index'
-  get 'signup', to: 'auth#signup'
-  get 'landing', to: 'auth#landing'
-  get 'home', to: 'auth#home'
-  get 'login', to: 'auth#login'
-
-
-  post "/login", to: "auth#login"
-  post "/signup", to: "auth#signup"
-  post "/logout", to: "auth#logout"
+  # root "articles#index"
 end
