632d9ef6c57691d2f2920919f92c2331dd1757d8
Harshith Latchupatula <harshithl1777@gmail.com>
2023-05-17 21:06:22 -0400

    Update 20230517233628_add_hideouts_table.rb

--- db/migrate/20230517233628_add_hideouts_table.rb
+++ db/migrate/20230517233628_add_hideouts_table.rb
@@ -1,7 +1,7 @@
 class AddHideoutsTable < ActiveRecord::Migration[7.0]
   def change
     create_table :hideouts do |t|
-      t.string :name, null: false, index: {unique: true}
+      t.string :name, null: false
       t.references :owner, null: false, foreign_key: {to_table: :users, on_delete: :cascade}, index: {unique: true}
     end
   end
