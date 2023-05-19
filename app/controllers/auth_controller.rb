72468791b1f8fdaf0084b74804504935b00d00d2
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-05-18 22:23:55 -0400

    fixed comments

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -12,32 +12,46 @@ class AuthController < ApplicationController
         email = params[:email]
         password = params[:password]
 
-        new_user = User.new(first_name: first_name, last_name: last_name, email: email)
-        new_user.save
+        if first_name.nil? || last_name.nil? || email.nil? || password.nil?
+            render status: 400
+            return
+        end
+
+        # new_user = User.new(first_name: first_name, last_name: last_name, email: email)
+        # new_user.save
 
         uri = URI("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=#{ENV['FIREBASE_API_KEY']}")
-        res = Net::HTTP.post_form(uri, 'email' => email, 'password' => password)
+        res = Net::HTTP.post_form(uri, 'email': email, 'password': password)
         data = JSON.parse(res.body)
 
-        puts User.all
+        puts data
 
         if data.member?('error')
-            render status: 404
+            render status: 400, body: data['error']['message']
         else
             render status: :created
         end
+
+        render :json {'first_name': first_name, 'last_name': last_name, 'email': email}
     end
 
     def login
         email = params[:email]
         password = params[:password]
 
+        if email.nil? || password.nil?
+            render status: 400
+            return
+        end
+
         uri = URI("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=#{ENV['FIREBASE_API_KEY']}")
-        res = Net::HTTP.post_form(uri, 'email' => email, 'password' => password)
+        res = Net::HTTP.post_form(uri, 'email': email, 'password': password)
         data = JSON.parse(res.body)
 
+        puts data
+
         if data.member?('error')
-            render status: 404
+            render status: 401, body: data['error']['message']
         else
             render status: 200
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   root "auth#signup"
 
-  get '/', to: 'auth#signup'
-  get 'signup', to: 'auth#signup'
-  get 'landing', to: 'auth#landing'
-  get 'home', to: 'auth#home'
-  get 'login', to: 'auth#login'
-
-
   post "/sessions", to: "auth#login"
   post "/users", to: "auth#signup"
-  post "/delete/session", to: "auth#logout"
+  delete "/sessions", to: "auth#logout"
 end
