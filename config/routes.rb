7add8b18dc3c9c86255f610c47ff71b0ea11cd8c
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-05-23 14:12:20 -0400

    fix: corrected pluralization

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   post "/users", to: "auth#signup"
   delete "/sessions", to: "auth#logout"
 
-  post '/hideout', to: "hideouts#create"
-  delete 'hideout', to: "hideouts#destroy"
-  put 'hideout/rename', to: "hideouts#rename"
-  put "/hideout/users", to: "hideouts#add"
-  delete "/hideout/users", to: "hideouts#leave"
-  delete "/hideout/destroy", to: "hideouts#destroy"
+  post '/hideouts', to: "hideout#create"
+  delete '/hideouts', to: "hideout#destroy"
+  put '/hideouts/rename', to: "hideout#rename"
+  put "/hideouts/users", to: "hideout#add"
+  delete "/hideouts/users", to: "hideout#leave"
+  delete "/hideouts/destroy", to: "hideout#destroy"
 
 end
 
