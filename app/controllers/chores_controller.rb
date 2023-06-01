0ab1335a7fba498da62654727256ec7674be9f96
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-05-31 20:39:19 -0400

    implemented requested changes

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,49 +1,38 @@
 class ChoresController < ApplicationController
   def create
+    params.require(:chore).permit(:title, :hideout_id)
+
     title = params[:title]
     description = params[:description]
     due_date = params[:due_date]
     assignee_email = params[:assignee_email]
     hideout_id = params[:hideout_id]
-
-    if title.nil? || hideout_id.nil?
-      render status: 400
+   
+    if assignee_email.nil?
+      assignee_id = nil
     else
-      if assignee_email.nil?
-        assignee_id = nil
-      else
-        assignee_id = User.find_by(email: assignee_email).id
-      end
-      chore = Chore.create(title: title, description: description, due_date: due_date, assignee_id: assignee_id, hideout_id: hideout_id)
-      chore.save
-      render status: :created, :json => {title: title, description: description, due_date: due_date, assignee_email: assignee_email, hideout_id: hideout_id}
+      assignee_id = User.find_by(email: assignee_email).id
     end
+    
+    chore = Chore.create(title: title, description: description, due_date: due_date, assignee_id: assignee_id, hideout_id: hideout_id)
+    chore.save
+    render status: 201, :json => {title: title, description: description, due_date: due_date, assignee_email: assignee_email, hideout_id: hideout_id}
+    
   end
 
   def update
     params.require(:id)
     id = params[:id]
-
-    # TODO: add middleware for checking if user exists and chores exists 
-
+  
+    # TODO: add middleware for checking if user exists and chore exists
+  
     chore = Chore.find_by(id: id)
-
-    if params.has_key?(:title)
-      chore.update(title: params[:title])
+  
+    if chore.update(params.require(:chore).permit(:title, :description, :due_date, :assignee_email, :hideout_id))
+      render status: 200
     end
-    if params.has_key?(:description)
-      chore.update(description: params[:description])
-    end
-    if params.has_key?(:due_date)
-      chore.update(due_date: params[:due_date])
-    end
-    if params.has_key?(:assignee_email)
-      chore.update(assignee_id: User.find_by(email: params[:assignee_email]).id)
-    end
-
-    render status: :ok
-
   end
+  
 
   def destroy
     params.require(:id)
@@ -51,6 +40,6 @@ class ChoresController < ApplicationController
 
     Chore.find_by(id: id).destroy
 
-    render status: :ok
+    render status: 200
   end
 end
