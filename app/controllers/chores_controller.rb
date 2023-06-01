cb06d74500e856a2c125959f2f8ef85c8e9ab1cd
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-05-31 21:29:59 -0400

    fixed begin-rescue block

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,27 +1,34 @@
 class ChoresController < ApplicationController
   def create
     begin
-      params.require(:chore).permit(:title, :hideout_id)
+      params.require([:chore, :title, :hideout_id])
+      title = params[:title]
+      description = params[:description]
+      due_date = params[:due_date]
+      assignee_email = params[:assignee_email]
+      hideout_id = params[:hideout_id]
+  
+      if assignee_email.nil?
+        assignee_id = nil
+      else
+        assignee_id = User.find_by(email: assignee_email).id
+      end
+  
+      chore = Chore.create(title: title, description: description, due_date: due_date, assignee_id: assignee_id, hideout_id: hideout_id)
+      chore.save
+  
+      render status: 201, json: {
+        title: title,
+        description: description,
+        due_date: due_date,
+        assignee_email: assignee_email,
+        hideout_id: hideout_id
+      }
     rescue ActionController::ParameterMissing
       render status: 400
-
-    title = params[:title]
-    description = params[:description]
-    due_date = params[:due_date]
-    assignee_email = params[:assignee_email]
-    hideout_id = params[:hideout_id]
-   
-    if assignee_email.nil?
-      assignee_id = nil
-    else
-      assignee_id = User.find_by(email: assignee_email).id
+      return
     end
-    
-    chore = Chore.create(title: title, description: description, due_date: due_date, assignee_id: assignee_id, hideout_id: hideout_id)
-    chore.save
-    render status: 201, :json => {title: title, description: description, due_date: due_date, assignee_email: assignee_email, hideout_id: hideout_id}
-    
-  end
+  end  
 
   def update
     params.require(:id)
