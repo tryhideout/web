14d0b063c56865f722cef7a52af45b4ac6fda1ce
Harshith <harshithl1777@gmail.com>
2023-06-03 22:05:47 -0400

    feat: add access token parsing and reconcile hideout_id in request params

--- lib/middleware/verify_access_token.rb
+++ lib/middleware/verify_access_token.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 require_relative '../../app/helpers/auth_helper.rb'
 
 module Middleware
@@ -11,11 +13,21 @@ module Middleware
       request_identifier = "#{request.method} #{request.path}"
       if not ENV['PUBLIC_ROUTES'].include?(request_identifier)
         authoriation_header = request.headers[:Authorization]
-        return 400, {}, [] if authoriation_header.nil?
+        return 401, {}, [] if authoriation_header.nil?
 
         access_token = authoriation_header.split[1]
         result = AuthHelper.validate_token_by_type(:ACCESS, access_token)
         return 401, {}, [] if not result[:success]
+
+        begin
+          payload = result[:payload]
+          payload.transform_keys!(&:to_sym)
+          current_user = User.find_by!(email: payload[:email])
+          payload[:hideout_id] = current_user[:hideout_id]
+          request.update_param(:payload, result[:payload])
+        rescue ActiveRecord::RecordNotFound
+          return 400, {}, []
+        end
       end
 
       @app.call(env)
