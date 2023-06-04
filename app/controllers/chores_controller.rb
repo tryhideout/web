14fbba88ce2e95d7d8701a0ad00a350b7b6e063e
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-06-03 21:46:46 -0400

    implemented requested changes

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,37 +1,42 @@
 class ChoresController < ApplicationController
   def create
     begin
-      chore = Chore.create(chore_params)
+      #hideout_id = params[:payload]
+      params.require(%i[title description hideout_id assignee_email due_date])
+      assignee_id = User.find_by(id: params[:assignee_email])
+      chore = Chore.create(title: params[:title], description: params[:description], hideout_id: params[:hideout_id], assignee_id: params[:assignee_id], due_date: params[:due_date])
       render status: 201,  json: chore.to_json
-    rescue
+    rescue ActionController::ParameterMissing => e
+      puts e
       render status: 400
     end
   end
 
-  def destroy
-    begin
-      params.require(:id)
-      Chore.destroy_by(id: params[:id])
-      render status: 200
-    rescue
-      render status: 400
-    end
-  end
-
+  
   def update
+    #Assume that user, chore and hideout exist
+
     begin
-      id = params[:id]  
+      #hideout_id = params[:payload]
+      id = params[:id] 
+      params.require(%i[title description hideout_id assignee_email due_date]) 
+      assignee_id = User.find_by(id: params[:assignee_email])
       chore = Chore.find_by(id: id)    
-      chore.update(chore_params)
+      chore.update(title: params[:title], description: params[:description], hideout_id: params[:hideout_id], assignee_id: params[:assignee_id], due_date: params[:due_date])
       render status: 200
     rescue
       render status: 400
     end
   end
 
-  private
-
-  def chore_params
-    params.require(:chore).permit(:title, :description, :hideout_id, :assignee_id, :due_date)
+def destroy
+  #Assume that user, chore and hideout exist
+  begin
+    params.require(:id)
+    Chore.destroy_by(id: params[:id])
+    render status: 200
+  rescue
+    render status: 400
   end
+end
 end
