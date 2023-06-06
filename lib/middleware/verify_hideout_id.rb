a699cce63870a866cbe9b17ba8056f1e09d2c588
Vikram Makkar <72366705+999Vik@users.noreply.github.com>
2023-06-06 18:18:04 -0400

    added check between the two hideout_id's

---
+++ lib/middleware/verify_hideout_id.rb
@@ -0,0 +1,39 @@
+require 'json'
+
+require_relative '../../app/helpers/auth_helper.rb'
+
+module Middleware
+    class VerifyHideoutId
+      def initialize(app)
+        @app = app
+      end
+  
+      def call(env)
+        request = ActionDispatch::Request.new(env)
+        request_identifier = "#{request.method} #{request.path}"
+        if not ENV['PUBLIC_ROUTES'].include?(request_identifier)
+          authoriation_header = request.headers[:Authorization]
+          return [401, {}, []] if authoriation_header.nil?
+  
+          access_token = authoriation_header.split[1]
+          result = AuthHelper.validate_token_by_type(:ACCESS, access_token)
+          return [401, {}, []] unless result[:success]
+  
+          begin
+            payload = result[:payload]
+            payload.transform_keys!(&:to_sym)
+            current_hideout = Hideout.find_by(id: payload[:hideout_id])
+            return [400, {}, []] unless current_hideout == request.params[:hideout_id].to_i
+  
+            request.update_param(:payload, result[:payload])
+  
+          rescue ActiveRecord::RecordNotFound
+            return [400, {}, []]
+          end
+        end
+  
+        @app.call(env)
+      end
+    end
+  end
+  
