12aa3aa98fec51a70a3309338c53b47b9db4eb6b
Harshith <harshithl1777@gmail.com>
2023-06-07 21:46:53 -0400

    feat: add boilerplate vue project

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+    root: true,
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier/skip-formatting'],
+    overrides: [
+        {
+            files: ['**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'],
+            extends: ['plugin:cypress/recommended'],
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+};
