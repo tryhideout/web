27ee06e107d1534c145d9127ba34af5c0cda634c
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-06-08 23:30:28 -0400

    feat: added specific rescue statements

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,7 +5,7 @@ class ExpensesController < ApplicationController
       create_params = params.require(:expense).permit([:name, :amount, :due_date, :debtor_id, :hideout_id, :comments])
       expense = Expense.create!(create_params)
       render status: 201, json: expense.to_json
-    rescue
+    rescue ActionController::ParameterMissing, ActiveModel::StrictValidationFailed
       render status: 400
     end
   end
@@ -25,8 +25,10 @@ class ExpensesController < ApplicationController
       update_params = params.require(:expense).permit([:name, :amount, :due_date, :debtor_id, :hideout_id, :comments])
       expense = Expense.find!(params[:id])    
       expense.update!(expense_params)
-      render status: 200
-    rescue
+      render status: 200  
+    rescue ActiveRecord::RecordNotFound
+      render status: 404
+    rescue ActionController::ParameterMissing, ActiveModel::StrictValidationFailed 
       render status: 400
     end
   end
