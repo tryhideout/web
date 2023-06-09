3c9615c8831f87eff5a8cde56f46d5c416e46fd7
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-06-08 23:12:26 -0400

    feat: returned 200 status from successful update

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -24,6 +24,7 @@ class ExpensesController < ApplicationController
       update_params = params.require(:expense).permit([:name, :amount, :due_date, :debtor_id, :hideout_id, :comments])
       expense = Expense.find!(params[:id])    
       expense.update!(expense_params)
+      render status: 200
     rescue
       render status: 400
     end
