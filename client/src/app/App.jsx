31f1822c5888bafeb859857863106748c3fdd522
Harshith <harshithl1777@gmail.com>
2023-06-19 21:57:12 -0400

    fix: remove unnecessary imports

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,13 +1,11 @@
-import { ChakraProvider, Box, Input, Button } from '@chakra-ui/react';
+import { ChakraProvider, Box } from '@chakra-ui/react';
 import theme from 'config/theme';
 import fonts from 'config/fonts';
 
 const App = () => {
 	return (
 		<ChakraProvider theme={theme}>
-			<Box bg='gradient' fontFamily='heading' fontWeight='400'>
-				Welcome
-			</Box>
+			<Box>Hello World</Box>
 		</ChakraProvider>
 	);
 };
