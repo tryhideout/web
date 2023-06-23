6956694dc91d0914baf74cc95c08b3cdde5f4c01
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-06-22 20:17:41 -0400

    feat: added routing to /auth/signup

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,13 +1,19 @@
 import { ChakraProvider, Box } from '@chakra-ui/react';
 import theme from 'config/theme';
 import fonts from 'config/fonts';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from 'pages/Signup';
 
 const App = () => {
-	return (
-		<ChakraProvider theme={theme}>
-			<Box>Hello World</Box>
-		</ChakraProvider>
-	);
+    return (
+        <ChakraProvider theme={theme}>
+            <Router>
+                <Routes>
+                    <Route path="/auth/signup" element={<Signup />} />
+                </Routes>
+            </Router>
+        </ChakraProvider>
+    );
 };
 
 export default App;
