d2f503236bc542892e382829340de3cd8c6f0db5
Harshith <harshithl1777@gmail.com>
2023-07-27 20:11:07 -0400

    feat: add active and status columns to expenses and chores tables, update controllers

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,5 +1,6 @@
 class Chore < ActiveRecord::Base
   self.table_name = 'chores'
+  enum status: { backlog: 'backlog', in_progress: 'in_progress', completed: 'completed' }
   belongs_to :hideout, foreign_key: 'hideout_id'
   belongs_to :user, foreign_key: 'assignee_id'
 
