e6144ecf0505142ac504aa3d255d4cfc1243e817
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2023-08-03 19:04:38 -0400

    feat: added all main pages to routes

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { Dashboard, LandingPage } from 'pages';
+import { Chores, Dashboard, Expenses, LandingPage } from 'pages';
 import { LoginPage } from 'pages';
 import theme from 'config/theme';
 import fonts from 'config/fonts.css';
@@ -16,6 +16,8 @@ const App = () => {
 					<Route path='/auth/login' element={<LoginPage />} />
                     <Route path="/auth/signup" element={<Signup />} />
                     <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/expenses" element={<Expenses />} />
+                    <Route path="/chores" element={<Chores />} />
 				</Routes>
 			</Router>
 		</ChakraProvider>
