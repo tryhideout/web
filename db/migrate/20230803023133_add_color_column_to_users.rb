5cf825b9000b771382690d1c6a6bd87848620d57
Harshith <harshithl1777@gmail.com>
2023-08-03 22:42:54 -0400

    feat: add color column to users table, random color selection on join, hideout member limit of 6

---
+++ db/migrate/20230803023133_add_color_column_to_users.rb
@@ -0,0 +1,10 @@
+class AddColorColumnToUsers < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      ALTER TABLE users ADD color ENUM('red', 'blue', 'purple', 'yellow', 'green', 'orange');
+    SQL
+  end
+  def down
+    remove_column :users, :color
+  end
+end
