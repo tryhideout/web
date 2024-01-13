8fce25d1910d0da1fcf0973332af245893c77cf8
Harshith <harshithl1777@gmail.com>
2024-01-13 18:06:42 -0500

    remove .env file from git tracking

---
+++ client/src/config/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+
+interface FirebaseConfig {
+	apiKey: string;
+	authDomain: string;
+	projectId: string;
+	appId: string;
+}
+
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
